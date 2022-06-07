d17e4de62123d817c432dbcb2ab2025cf5086f7d
PC-Serbatic <samuel.isip62@gmail.com>
2022-06-07 18:49:41 +0200

    Design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./*.html", "./js/**/*.js", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    fontFamily: {
+      mono: ["Monotype Corsiva", "serif"],
+    },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease-out 0.2s 1 forwards",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
